03b0be22e7ee44a3e354866e2c2b4f637abff4ae
lazaruslarue <lazaruslarue@gmail.com>
2016-08-23 14:35:53 -0600

    new directive

--- public/js/typewriterDirective.js
+++ public/js/typewriterDirective.js
@@ -1,1 +1,21 @@
-typewriterDirective.js
+(function () {
+
+angular.module('extractionApp').directive('typeText', function function_name(argument) {
+  return {
+    template: '<div>{{typewriter.text}}</div>',
+    restrict: 'E',
+    templateNamespace: 'html',
+    controllerAs: 'typewriter',
+    scope: {
+      commandingText: "=commandingText"
+    },
+    controller: function($scope, $element, $attrs, ExtractionFactory) { 
+      
+
+    },
+    link: function link(scope, ele, attrs) {
+      
+    }
+  }
+}
+})()
